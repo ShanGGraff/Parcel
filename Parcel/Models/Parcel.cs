df96ed8241421a6b09c82181c446202506bc3980
Shane Graff <copellius@gmail.com>
2021-09-14 19:55:51 -0700

    Initialize project
    Co-authored-by: Gabriel Ayala <gabeayala100@gmail.com>

---
+++ Parcel/Models/Parcel.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Parcel.Models
+{
+  public class Parcel
+  {
+    //Need constructors
+    //getters and setters for each property
+    //should be able to create parcel and specify dimensions and weight in arguments
+    //volume method Volume() called on Parcel instance that should return the porduct of the sides
+      public int Width { get; set; }
+      public int Length { get; set; }
+      public int Height { get; set; }
+      public int Weight { get; set; }
+
+    public Parcel(int width, int length, int height, int weight) 
+    {
+      Width = width;
+      Length = length;
+      Height = height;
+      Weight = weight;
+
+    }
+
+  }
+}
